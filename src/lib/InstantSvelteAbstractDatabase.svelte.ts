42878f8c283199079614e475bef52fe6421ae179
wobsoriano <sorianorobertc@gmail.com>
2025-02-15 20:49:59 -0800

    chore: revert publishTopic return

--- src/lib/InstantSvelteAbstractDatabase.svelte.ts
+++ src/lib/InstantSvelteAbstractDatabase.svelte.ts
@@ -123,7 +123,7 @@ export class InstantSvelteRoom<
 	 */
 	usePublishTopic = <Topic extends keyof RoomSchema[RoomType]['topics']>(
 		topic: MaybeGetter<Topic>
-	): ReactiveValue<(data: RoomSchema[RoomType]['topics'][Topic]) => void> => {
+	): (data: RoomSchema[RoomType]['topics'][Topic]) => void => {
 		$effect(() => {
 			const unsubscribe = this._core._reactor.joinRoom(toValue(this.id));
 
@@ -139,11 +139,7 @@ export class InstantSvelteRoom<
 			});
 		};
 
-		return {
-			get current() {
-				return publishTopic;
-			}
-		};
+		return publishTopic;
 	};
 
 	/**
