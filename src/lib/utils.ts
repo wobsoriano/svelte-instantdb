ab77eaf22caddf3b270da25dd07aeee9d01abc9b
wobsoriano <sorianorobertc@gmail.com>
2025-02-15 20:47:48 -0800

    chore: return type clean up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,7 @@
 export type MaybeGetter<T> = T | (() => T);
+export type ReactiveValue<T> = {
+	readonly current: T;
+};
 
 export function toValue<T>(value: MaybeGetter<T>): T {
 	return typeof value === 'function' ? (value as () => T)() : value;
